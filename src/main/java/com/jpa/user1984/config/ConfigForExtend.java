ac06d8dbc43b2760f1e954a29266f8ab2ceba982
hanouw <jehasite@gmail.com>
2024-04-08 20:56:33 +0900

    시큐리티 오류 수정

--- src/main/java/com/jpa/user1984/config/ConfigForExtend.java
+++ src/main/java/com/jpa/user1984/config/ConfigForExtend.java
@@ -1,8 +1,6 @@
 package com.jpa.user1984.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -13,12 +11,11 @@ public class ConfigForExtend {
     @Bean
     public WebSecurityCustomizer WebSecurityCustomizer() {
         return (web) -> web.ignoring()
-//                .requestMatchers(new AntPathRequestMatcher("/**"))
                 .requestMatchers(new AntPathRequestMatcher("/css/**"))
                 .requestMatchers(new AntPathRequestMatcher("/cms/ajaxUsernameAvail"))
                 .requestMatchers(new AntPathRequestMatcher("/img/**"));
     }
-    //     비밀번호 암호화를 위한 도구
+    // 비밀번호 암호화를 위한 도구
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
