50fe8612a6e3c0417aed4edc86aa2999cbbaf0e2
Goldenhyo <jintop01@naver.com>
2024-04-22 19:21:47 +0900

    StoreStatus 수정

--- src/main/java/com/jpa/user1984/domain/StoreStatus.java
+++ src/main/java/com/jpa/user1984/domain/StoreStatus.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum StoreStatus {
-    STORE("STATUS_STORE"), QUIT("STATUS_QUIT") ;
+    STORE("STATUS_STORE"), QUIT("STATUS_QUIT"), ADMIN("STATUS_ADMIN") ;
     private final String value;
 }
 
